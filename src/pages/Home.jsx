64a971107960910f26c6c95a14fc6f5182623775
yachaechae <wpql1207@naver.com>
2023-11-16 17:50:37 +0900

    메인 와이어프레임 구축

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "components/Header";
+import InputForm from "components/InputForm";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+        <Header />
+        <InputForm></InputForm>
+    </>
+  );
+}
---
+++ src/shared/Router.js
@@ -0,0 +1,15 @@
+import Detail from "../pages/Detail";
+import Home from "../pages/Home";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Router() {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/detail" element={<Detail/>}/>
+        </Routes>
+    </BrowserRouter>
+    )
+}
