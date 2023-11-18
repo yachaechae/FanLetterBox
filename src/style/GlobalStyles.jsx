a4c74ee700102679d58640421ad0fbed9bbcfda6
yachaechae <wpql1207@naver.com>
2023-11-18 17:54:03 +0900

    폰트추가

--- src/style/GlobalStyles.jsx
+++ src/style/GlobalStyles.jsx
@@ -9,15 +9,22 @@
         font-weight: 400;
         font-style: normal;
     }
+    @font-face {
+    font-family: 'HakgyoansimWoojuR';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/HakgyoansimWoojuR.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
      a{
          text-decoration:none;
          color:inherit;
      }
      *{
-         box-sizing:border-box;
+        font-family: 'HakgyoansimWoojuR';
+        box-sizing:border-box;
      }
      body{
-         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'HakgyoansimWoojuR';
          font-size: 14px;
          background-color:rgba(255,255,255,1);
      }
