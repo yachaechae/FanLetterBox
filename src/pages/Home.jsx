d2f67610281b64af05977847cd316c5fc4186f08
yachaechae <wpql1207@naver.com>
2023-11-18 17:55:48 +0900

    state 위치 변경 및 localStorage, 팬레터 리스트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import Header from "components/Header";
-import InputForm from "components/InputForm";
-import React from "react";
+import LetterForm from "components/LetterForm";
+import LetterList from "components/LetterList";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [selectedId,setSelectedId] = useState("")
+  const [letterList, setLetterList] = useState([])
+  const [modalOpen,setModalOpen] = useState(false)
+  useEffect(() => {
+    setLetterList( JSON.parse(localStorage.getItem("letterList")))
+  }, [])
+  
   return (
     <>
-        <Header />
-        <InputForm></InputForm>
+        <Header selectedId = {selectedId} setSelectedId={setSelectedId}/>
+        <LetterForm letterList = {letterList} setLetterList={setLetterList} modalOpen = {modalOpen} setModalOpen={setModalOpen}/>
+        <LetterList selectedId = {selectedId} letterList={letterList}/>
     </>
   );
 }
